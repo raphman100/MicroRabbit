43a5090f1554bace597a7a62de2f3c4b97a20d8b
Raphael McAbee-Reher <raphman100@gmail.com>
2021-05-02 20:44:57 -0400

    Implemented Publish and SendCommand for Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Models/AccountTransfer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Models
+{
+  public class AccountTransfer
+  {
+    public int FromAccount { get; set; }
+    public int ToAccount { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")] 
+    public decimal TransferAmount { get; set; }
+  }
+}
